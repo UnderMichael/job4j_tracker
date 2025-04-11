9812849fecfd9768e1f48b3d8a37f63e8dcdd7e0
underMichael <m.podddorogin@gmail.com>
2025-04-11 16:26:02 +0300

    5. Tracker - хранилище

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,25 +1,53 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
-    private final Item[] items = new Item[100];
-    private int ids = 1;
-    private int size = 0;
+	private final Item[] items = new Item[100];
 
-    public Item add(Item item) {
-        item.setId(ids++);
-        items[size++] = item;
-        return item;
-    }
+	private int ids = 1;
 
-    public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
-    }
+	private int size = 0;
+
+	public Item add(Item item) {
+		item.setId(ids++);
+		items[size++] = item;
+		return item;
+	}
+
+	public Item[] findAll() {
+		Item[] result = new Item[items.length];
+		int size = 0;
+		for (Item item : this.items) {
+			if (item != null) {
+				result[size] = item;
+				size++;
+			}
+		}
+		return Arrays.copyOf(result, size);
+	}
+
+	public Item[] findByName(String key) {
+		Item[] result = new Item[items.length];
+		int size = 0;
+		for (Item item : this.items) {
+			if (item != null && item.getName().equals(key)) {
+				result[size] = item;
+				size++;
+			}
+		}
+		return Arrays.copyOf(result, size);
+	}
+
+	public Item findById(int id) {
+		Item rsl = null;
+		for (int index = 0; index < size; index++) {
+			Item item = items[index];
+			if (item != null && item.getId() == id) {
+				rsl = item;
+				break;
+			}
+		}
+		return rsl;
+	}
 }
