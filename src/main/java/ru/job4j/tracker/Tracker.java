a97588c8a17837d55487548e6141d791a4438db2
underMichael <m.podddorogin@gmail.com>
2025-04-11 16:38:12 +0300

    6. Метод замены заявки. Tracker.replace

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -16,7 +16,7 @@ public class Tracker {
 	}
 
 	public Item[] findAll() {
-		Item[] result = new Item[items.length];
+		Item[] result = new Item[this.size];
 		int size = 0;
 		for (Item item : this.items) {
 			if (item != null) {
@@ -28,7 +28,7 @@ public class Tracker {
 	}
 
 	public Item[] findByName(String key) {
-		Item[] result = new Item[items.length];
+		Item[] result = new Item[this.size];
 		int size = 0;
 		for (Item item : this.items) {
 			if (item != null && item.getName().equals(key)) {
@@ -40,14 +40,29 @@ public class Tracker {
 	}
 
 	public Item findById(int id) {
-		Item rsl = null;
+		int index = indexOf(id);
+		return index != -1 ? items[index] : null;
+	}
+
+	public boolean replace(int id, Item item) {
+		int index = indexOf(id);
+		if (index == -1) {
+			return false;
+		}
+		item.setId(id);
+		items[index] = item;
+
+		return true;
+	}
+
+	private int indexOf(int id) {
+		int result = -1;
 		for (int index = 0; index < size; index++) {
-			Item item = items[index];
-			if (item != null && item.getId() == id) {
-				rsl = item;
+			if (items[index].getId() == id) {
+				result = index;
 				break;
 			}
 		}
-		return rsl;
+		return result;
 	}
 }
