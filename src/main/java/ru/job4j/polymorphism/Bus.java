5d15d8c2a7046612267e8247cee6e6ae61085c63
underMichael <m.podddorogin@gmail.com>
2025-04-14 10:35:04 +0300

    4.2. Интерфейс Input

--- src/main/java/ru/job4j/polymorphism/Bus.java
+++ src/main/java/ru/job4j/polymorphism/Bus.java
@@ -1,18 +1,24 @@
 package ru.job4j.polymorphism;
 
 public class Bus implements Transport {
+	private static final int MAX_PASSENGERS = 60;
+
 	@Override
 	public void move() {
-
+		System.out.println("Bus start moving");
 	}
 
 	@Override
 	public void passengers(int count) {
-
+		if (count > MAX_PASSENGERS) {
+			System.out.println("there's no so much seats");
+		} else {
+			System.out.println("The bus departs with " + count + " passengers.");
+		}
 	}
 
 	@Override
 	public int refuel(int fuel) {
-		return 0;
+		return fuel * Transport.FUEL_COST;
 	}
 }
--- src/main/java/ru/job4j/polymorphism/Transport.java
+++ src/main/java/ru/job4j/polymorphism/Transport.java
@@ -1,6 +1,8 @@
 package ru.job4j.polymorphism;
 
 public interface Transport {
+	int FUEL_COST = 54;
+
 	void move();
 
 	void passengers(int count);
