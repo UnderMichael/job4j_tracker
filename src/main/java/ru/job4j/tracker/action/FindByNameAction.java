cf8994f55bc16949979cf4a78c699377e7f2868a
underMichael <m.podddorogin@gmail.com>
2025-04-20 16:25:31 +0300

    move item to package

--- src/main/java/ru/job4j/tracker/action/FindByNameAction.java
+++ src/main/java/ru/job4j/tracker/action/FindByNameAction.java
@@ -1,8 +1,8 @@
 package ru.job4j.tracker.action;
 
-import ru.job4j.tracker.Item;
 import ru.job4j.tracker.Tracker;
 import ru.job4j.tracker.input.Input;
+import ru.job4j.tracker.item.Item;
 import ru.job4j.tracker.output.Output;
 
 import java.util.List;
--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.tracker;
 
 import org.junit.jupiter.api.Test;
+import ru.job4j.tracker.item.Item;
 
 import java.util.List;
 
