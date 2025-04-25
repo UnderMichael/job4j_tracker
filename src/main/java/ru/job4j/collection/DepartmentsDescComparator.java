28bf3ea941fe6216507b2e75c5057ac27892f1cd
underMichael <m.podddorogin@gmail.com>
2025-04-25 11:10:41 +0300

    3. Отсортировать департаменты

--- src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -7,15 +7,10 @@ public class DepartmentsDescComparator implements Comparator<String> {
 	public int compare(String o1, String o2) {
 		String[] o1Parts = o1.split("/");
 		String[] o2Parts = o2.split("/");
-		if (!o1Parts[0].equals(o2Parts[0])) {
-			return o2Parts[0].compareTo(o1Parts[0]);
+		int result = o2Parts[0].compareTo(o1Parts[0]);
+		if (result != 0) {
+			return result;
 		}
-		int minLength = Math.min(o1Parts.length, o2Parts.length);
-		for (int i = 1; i < minLength; i++) {
-			if (!o2Parts[i].equals(o1Parts[i])) {
-				return o1Parts[i].compareTo(o2Parts[i]);
-			}
-		}
-		return Integer.compare(o1Parts.length, o2Parts.length);
+		return o1.compareTo(o2);
 	}
 }
