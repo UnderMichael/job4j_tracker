99e064e3750a7b67dad656ef3d803b1a71fc31b0
underMichael <m.podddorogin@gmail.com>
2025-04-30 13:43:33 +0300

    1. Метод .of

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -9,177 +9,177 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class DepartmentsTest {
 
-	@Test
-	void whenMissed() {
-		List<String> input = List.of("k1/sk1", "k2/sk1");
-		List<String> expected = List.of("k1", "k1/sk1", "k2", "k2/sk1");
-		List<String> result = Departments.fillGaps(input);
-		assertThat(result).containsSequence(expected);
-	}
+    @Test
+    void whenMissed() {
+        List<String> input = Arrays.asList("k1/sk1", "k2/sk1");
+        List<String> expected = List.of("k1", "k1/sk1", "k2", "k2/sk1");
+        List<String> result = Departments.fillGaps(input);
+        assertThat(result).containsSequence(expected);
+    }
 
-	@Test
-	void whenNonChange() {
-		List<String> input = List.of("k1", "k1/sk1");
-		List<String> expected = List.of("k1", "k1/sk1");
-		List<String> result = Departments.fillGaps(input);
-		assertThat(result).containsSequence(expected);
-	}
+    @Test
+    void whenNonChange() {
+        List<String> input = Arrays.asList("k1", "k1/sk1");
+        List<String> expected = List.of("k1", "k1/sk1");
+        List<String> result = Departments.fillGaps(input);
+        assertThat(result).containsSequence(expected);
+    }
 
-	@Test
-	void whenMissedSomeDepartmentsCode() {
-		List<String> input = List.of(
-				"K1/SK1/SSK1",
-				"K1/SK1/SSK2",
-				"K2/SK1/SSK1"
-		);
-		List<String> expected = List.of(
-				"K1",
-				"K1/SK1",
-				"K1/SK1/SSK1",
-				"K1/SK1/SSK2",
-				"K2",
-				"K2/SK1",
-				"K2/SK1/SSK1"
-		);
-		List<String> result = Departments.fillGaps(input);
-		assertThat(result).containsSequence(expected);
-	}
+    @Test
+    void whenMissedSomeDepartmentsCode() {
+        List<String> input = Arrays.asList(
+                "K1/SK1/SSK1",
+                "K1/SK1/SSK2",
+                "K2/SK1/SSK1"
+        );
+        List<String> expected = List.of(
+                "K1",
+                "K1/SK1",
+                "K1/SK1/SSK1",
+                "K1/SK1/SSK2",
+                "K2",
+                "K2/SK1",
+                "K2/SK1/SSK1"
+        );
+        List<String> result = Departments.fillGaps(input);
+        assertThat(result).containsSequence(expected);
+    }
 
-	@Test
-	void whenSortAscWithoutMissedDepartments() {
-		List<String> input = Arrays.asList(
-				"K1/SK1",
-				"K1/SK1/SSK1",
-				"K1/SK1/SSK2",
-				"K2",
-				"K2/SK1",
-				"K1/SK2",
-				"K1",
-				"K2/SK1/SSK2",
-				"K2/SK1/SSK1"
-		);
-		List<String> expected = List.of(
-				"K1",
-				"K1/SK1",
-				"K1/SK1/SSK1",
-				"K1/SK1/SSK2",
-				"K1/SK2",
-				"K2",
-				"K2/SK1",
-				"K2/SK1/SSK1",
-				"K2/SK1/SSK2"
-		);
-		Departments.sortAsc(input);
-		assertThat(input).containsSequence(expected);
-	}
+    @Test
+    void whenSortAscWithoutMissedDepartments() {
+        List<String> input = Arrays.asList(
+                "K1/SK1",
+                "K1/SK1/SSK1",
+                "K1/SK1/SSK2",
+                "K2",
+                "K2/SK1",
+                "K1/SK2",
+                "K1",
+                "K2/SK1/SSK2",
+                "K2/SK1/SSK1"
+        );
+        List<String> expected = List.of(
+                "K1",
+                "K1/SK1",
+                "K1/SK1/SSK1",
+                "K1/SK1/SSK2",
+                "K1/SK2",
+                "K2",
+                "K2/SK1",
+                "K2/SK1/SSK1",
+                "K2/SK1/SSK2"
+        );
+        Departments.sortAsc(input);
+        assertThat(input).containsSequence(expected);
+    }
 
-	@Test
-	void whenSortAscWithMissedDepartments() {
-		List<String> input = Arrays.asList(
-				"K1/SK1/SSK1",
-				"K1/SK1/SSK2",
-				"K2/SK1",
-				"K1/SK2",
-				"K2/SK1/SSK2",
-				"K2/SK1/SSK1"
-		);
-		List<String> expected = List.of(
-				"K1/SK1/SSK1",
-				"K1/SK1/SSK2",
-				"K1/SK2",
-				"K2/SK1",
-				"K2/SK1/SSK1",
-				"K2/SK1/SSK2"
-		);
-		Departments.sortAsc(input);
-		assertThat(input).containsSequence(expected);
-	}
+    @Test
+    void whenSortAscWithMissedDepartments() {
+        List<String> input = Arrays.asList(
+                "K1/SK1/SSK1",
+                "K1/SK1/SSK2",
+                "K2/SK1",
+                "K1/SK2",
+                "K2/SK1/SSK2",
+                "K2/SK1/SSK1"
+        );
+        List<String> expected = List.of(
+                "K1/SK1/SSK1",
+                "K1/SK1/SSK2",
+                "K1/SK2",
+                "K2/SK1",
+                "K2/SK1/SSK1",
+                "K2/SK1/SSK2"
+        );
+        Departments.sortAsc(input);
+        assertThat(input).containsSequence(expected);
+    }
 
-	@Test
-	void whenSortDescWithoutMissedDepartments() {
-		List<String> input = Arrays.asList(
-				"K1/SK1",
-				"K1/SK1/SSK1",
-				"K1/SK1/SSK2",
-				"K2",
-				"K2/SK1",
-				"K1/SK2",
-				"K1",
-				"K2/SK1/SSK2",
-				"K2/SK1/SSK1"
-		);
-		List<String> expected = List.of(
-				"K2",
-				"K2/SK1",
-				"K2/SK1/SSK1",
-				"K2/SK1/SSK2",
-				"K1",
-				"K1/SK1",
-				"K1/SK1/SSK1",
-				"K1/SK1/SSK2",
-				"K1/SK2"
-		);
-		Departments.sortDesc(input);
-		assertThat(input).containsSequence(expected);
-	}
+    @Test
+    void whenSortDescWithoutMissedDepartments() {
+        List<String> input = Arrays.asList(
+                "K1/SK1",
+                "K1/SK1/SSK1",
+                "K1/SK1/SSK2",
+                "K2",
+                "K2/SK1",
+                "K1/SK2",
+                "K1",
+                "K2/SK1/SSK2",
+                "K2/SK1/SSK1"
+        );
+        List<String> expected = List.of(
+                "K2",
+                "K2/SK1",
+                "K2/SK1/SSK1",
+                "K2/SK1/SSK2",
+                "K1",
+                "K1/SK1",
+                "K1/SK1/SSK1",
+                "K1/SK1/SSK2",
+                "K1/SK2"
+        );
+        Departments.sortDesc(input);
+        assertThat(input).containsSequence(expected);
+    }
 
-	@Test
-	void whenSortDescWithMissedDepartments() {
-		List<String> input = Arrays.asList(
-				"K1/SK1",
-				"K1/SK1/SSK1",
-				"K1/SK1/SSK2",
-				"K2/SK1",
-				"K1/SK2",
-				"K2/SK1/SSK2",
-				"K2/SK1/SSK1"
-		);
-		List<String> expected = List.of(
-				"K2/SK1",
-				"K2/SK1/SSK1",
-				"K2/SK1/SSK2",
-				"K1/SK1",
-				"K1/SK1/SSK1",
-				"K1/SK1/SSK2",
-				"K1/SK2"
-		);
-		Departments.sortDesc(input);
-		assertThat(input).containsSequence(expected);
-	}
+    @Test
+    void whenSortDescWithMissedDepartments() {
+        List<String> input = Arrays.asList(
+                "K1/SK1",
+                "K1/SK1/SSK1",
+                "K1/SK1/SSK2",
+                "K2/SK1",
+                "K1/SK2",
+                "K2/SK1/SSK2",
+                "K2/SK1/SSK1"
+        );
+        List<String> expected = List.of(
+                "K2/SK1",
+                "K2/SK1/SSK1",
+                "K2/SK1/SSK2",
+                "K1/SK1",
+                "K1/SK1/SSK1",
+                "K1/SK1/SSK2",
+                "K1/SK2"
+        );
+        Departments.sortDesc(input);
+        assertThat(input).containsSequence(expected);
+    }
 
-	@Test
-	void compare() {
-		int result = new DepartmentsDescComparator().compare(
-				"K2/SK1/SSK2",
-				"K2/SK1/SSK1"
-		);
-		assertThat(result).isGreaterThan(0);
-	}
+    @Test
+    void compare() {
+        int result = new DepartmentsDescComparator().compare(
+                "K2/SK1/SSK2",
+                "K2/SK1/SSK1"
+        );
+        assertThat(result).isGreaterThan(0);
+    }
 
-	@Test
-	void whenUpDepartmentGoBefore() {
-		int result = new DepartmentsDescComparator().compare(
-				"K2",
-				"K2/SK1"
-		);
-		assertThat(result).isLessThan(0);
-	}
+    @Test
+    void whenUpDepartmentGoBefore() {
+        int result = new DepartmentsDescComparator().compare(
+                "K2",
+                "K2/SK1"
+        );
+        assertThat(result).isLessThan(0);
+    }
 
-	@Test
-	void whenFirstDepartmentIsDifferentThenAsc() {
-		int result = new DepartmentsDescComparator().compare(
-				"K2/SK1/SSK1",
-				"K1/SK1/SSK2"
-		);
-		assertThat(result).isLessThan(0);
-	}
+    @Test
+    void whenFirstDepartmentIsDifferentThenAsc() {
+        int result = new DepartmentsDescComparator().compare(
+                "K2/SK1/SSK1",
+                "K1/SK1/SSK2"
+        );
+        assertThat(result).isLessThan(0);
+    }
 
-	@Test
-	void whenFirstDepartmentIsDifferentThenDesc() {
-		int result = new DepartmentsDescComparator().compare(
-				"K1/SK1/SSK1",
-				"K2/SK1"
-		);
-		assertThat(result).isGreaterThan(0);
-	}
+    @Test
+    void whenFirstDepartmentIsDifferentThenDesc() {
+        int result = new DepartmentsDescComparator().compare(
+                "K1/SK1/SSK1",
+                "K2/SK1"
+        );
+        assertThat(result).isGreaterThan(0);
+    }
 }
